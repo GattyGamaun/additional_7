35402a88511178a187e119c357f226ae4d5c8bbb
Sergio Kondratiuk <gattygamaun@gmail.com>
2018-10-04 16:49:02 +0300

    sudoku/fix bugs: new soluton

--- src/index.js
+++ src/index.js
@@ -1,171 +1,124 @@
-let matrix = [
-  [6, 5, 0, 7, 3, 0, 0, 8, 0],
-  [0, 0, 0, 4, 8, 0, 5, 3, 0],
-  [8, 4, 0, 9, 2, 5, 0, 0, 0],
-  [0, 9, 0, 8, 0, 0, 0, 0, 0],
-  [5, 3, 0, 2, 0, 9, 6, 0, 0],
-  [0, 0, 6, 0, 0, 0, 8, 0, 0],
-  [0, 0, 9, 0, 0, 0, 0, 0, 6],
-  [0, 0, 7, 0, 0, 0, 0, 5, 0],
-  [1, 6, 5, 3, 9, 0, 4, 7, 0]
-];
+module.exports = function solveSudoku(matrix) {
 
-let column = 0;
-let rower = 0;
-let rowCube = 0;
-let colCube = 0;
-console.log(solveSudoku(matrix));
-// console.log("isUsedBox: " + isUsedBox(0, 0, 9));
+    let tempMatrix = copyMatrix(matrix);
+    let indexesOfZero = findIndexesOfZero(tempMatrix);
+    let solvedSudoku = solveSudoku(tempMatrix, indexesOfZero);
 
-function solveSudoku() {
-  if (!findUnsigned()) {
-    return true;
-  }
-  for (let num = 1; num <= 9; num++) {
-    if (isSafe(num)) {
-      matrix[rower][column] = num;
-      let rowRem = rower;
-      let colRem = column;
-      let valRem = num
-      if (solveSudoku(matrix)) {
-        console.log(matrix)
-        return matrix;
-      } else {
-        matrix[rower][column] = 0;
-        if (valRem < 9){
-          matrix[rowRem][colRem] = valRem + 1;
-        }else {
-          matrix[rowRem][colRem] = 1;
+    //copy to temp matrix
+    function copyMatrix(table) {
+        const ar = [];
+        for (let i = 0; i < 9; i++) {
+            ar.push(table[i]);
         }
-        
-        if (solveSudoku(matrix)) {
-          return matrix;
+        return ar;
+    }
+
+    //find indexes of zero
+    function findIndexesOfZero(table) {
+        let zeroPositions = [];
+        for (let i = 0; i < table.length; i++) {
+            for (let j = 0; j < table[i].length; j++) {
+                let tempAr = [];
+                if (table[i][j] === 0) {
+                    tempAr.push(i);
+                    tempAr.push(j);
+                    zeroPositions.push(tempAr);
+                }
+            }
         }
-      }
+        return zeroPositions;
     }
-  }
-  console.log(matrix);
 
-  return false;
-}
-
-function isSafe(digit, row, col) {
-  return !isUsedRow(digit) &&
-    !isUsedColumn(digit) &&
-    !isUsedBox(digit);
-}
-
-function isUsedBox(digit) {
-  let isBox = null;
-  let box = [];
-  if ((rowCube < 3) && (colCube < 3)) {
-    for (let k = 0; k < 3; k++) {
-      for (let i = 0; i < 3; i++) {
-        box.push(matrix[k][i]);
-      }
-    }
-  } else if ((rowCube < 3) && (colCube > 2 && colCube < 6)) {
-    for (let k = 0; k < 3; k++) {
-      for (let i = 3; i < 6; i++) {
-        box.push(matrix[k][i]);
-      }
-    }
-  } else if ((rowCube < 3) && (colCube > 5)) {
-    for (let k = 0; k < 3; k++) {
-      for (let i = 6; i < 9; i++) {
-        box.push(matrix[k][i]);
-      }
-    }
-  } else if ((rowCube > 2 && rowCube < 6) && (colCube < 3)) {
-    for (let k = 3; k < 6; k++) {
-      for (let i = 0; i < 3; i++) {
-        box.push(matrix[k][i]);
-      }
-    }
-  } else if ((rowCube > 2 && rowCube < 6) && (colCube > 2 && colCube < 6)) {
-    for (let k = 3; k < 6; k++) {
-      for (let i = 3; i < 6; i++) {
-        box.push(matrix[k][i]);
-      }
-    }
-  } else if ((rowCube > 2 && rowCube < 6) && (colCube > 5)) {
-    for (let k = 3; k < 6; k++) {
-      for (let i = 6; i < 9; i++) {
-        box.push(matrix[k][i]);
-      }
-    }
-  } else if ((rowCube > 5) && (colCube < 3)) {
-    for (let k = 6; k < 9; k++) {
-      for (let i = 0; i < 3; i++) {
-        box.push(matrix[k][i]);
-      }
-    }
-  } else if ((rowCube > 5) && (colCube > 2 && colCube < 6)) {
-    for (let k = 6; k < 9; k++) {
-      for (let i = 3; i < 6; i++) {
-        box.push(matrix[k][i]);
-      }
-    }
-  } else {
-    for (let k = 6; k < 9; k++) {
-      for (let i = 6; i < 9; i++) {
-        box.push(matrix[k][i]);
-      }
-    }
-  }
-
-  isBox = box.some(function (x) {
-    return x === digit;
-  });
-  console.log(box);
-  return isBox;
-}
-
-function findUnsigned() {
-  for (let rowPos = 0; rowPos < 9; rowPos++) {
-    for (let colPos = 0; colPos < 9; colPos++) {
-      let value = matrix[rowPos][colPos];
-      rowCube = rowPos;
-      colCube = colPos;
-      if (value === 0) {
-        rower = rowPos;
-        column = colPos;
+    //is used passing number in the row
+    function isUsedRow(table, row, number) {
+        for (let i = 0; i < table[row].length; i++) {
+            if (table[row][i] === number) {
+                return false;
+            }
+        }
         return true;
-      }
     }
-  }
-  return false;
-}
 
-function isSudokuFull(matrix) {
-  let isFull = null;
-  let boolmatrix = [];
-  for (let i = 0; i < 9; i++) {
-    boolmatrix[i] = matrix[i].every(function (x) {
-      return (x !== 0);
-    });
-  }
-  isFull = boolmatrix.every(function (value) {
-    return value === true;
-  })
-  return isFull;
-}
+    //is used passing number in the column
+    function isUsedColumn(table, column, number) {
+        for (let i = 0; i < 9; i++) {
+            if (table[i][column] === number) {
+                return false
+            }
+        }
+        return true;
+    }
 
-function isUsedRow(digit) {
-  let isRow = matrix[rower].some(function (x) {
-    return x === digit;
-  });
-  return isRow;
-}
+    //is used passing number in the 3x3 box
+    function isUsedBox(table, column, row, number) {
 
-function isUsedColumn(digit) {
-  let isColumn = null;
-  let columnAr = [];
-  for (let i = 0; i < 9; i++) {
-    columnAr[i] = matrix[i][column];
-  }
-  isColumn = columnAr.some(function (x) {
-    return x === digit;
-  });
-  return isColumn;
+        let columnCorner = 0;
+        let rowCorner = 0;
+
+        while (column >= columnCorner + 3) {
+            columnCorner += 3;
+        }
+
+        while (row >= rowCorner + 3) {
+            rowCorner += 3;
+        }
+
+        for (let i = rowCorner; i < rowCorner + 3; i++) {
+            for (let j = columnCorner; j < columnCorner + 3; j++) {
+                if (tempMatrix[i][j] === number) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    //is used passing number in the each position
+    function isSafeToAdd(table, column, row, number) {
+        if (isUsedBox(table, column, row, number) && isUsedColumn(table, column, number)
+            && isUsedRow(table, row, number)) {
+            return true;
+        }
+        return false;
+    }
+
+    //inserting numbers into zeros elements
+    function solveSudoku(table, indexesOfZero) {
+
+        let maxValue = 9;
+
+        for (let i = 0; i < indexesOfZero.length;) {
+            let row = indexesOfZero[i][0];
+            let column = indexesOfZero[i][1];
+            // Try the next value
+            let value = table[row][column] + 1;
+            // Was a valid number found?
+            let found = false;
+            // Keep trying new values until either the maxValue
+            // was reached or a valid value was found
+            while (!found && value <= maxValue) {
+                // If a valid value is found, mark found true,
+                // set the position to the value, and move to the
+                // next position
+                if (isSafeToAdd(table, column, row, value)) {
+                    found = true;
+                    table[row][column] = value;
+                    i++;
+                }
+                // Otherwise, try the next value
+                else {
+                    value++;
+                }
+            }
+            // If no valid value was found and the maxValue was
+            // reached, move back to the previous position
+            if (!found) {
+                table[row][column] = 0;
+                i--;
+            }
+        }
+        return table;
+    }
+
+    return solvedSudoku;
 }
